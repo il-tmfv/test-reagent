73212c630e92c4155d2651738a15dc1cf1e451e1
Ilya Timofeev <il.tmfv@gmail.com>
2018-09-19 22:40:55 +0300

    Add test for add-watch for local state

--- src/test_reagent/core.cljs
+++ src/test_reagent/core.cljs
@@ -4,8 +4,9 @@
 
 (enable-console-print!)
 
-(defonce app-state* (r/atom {:count 0}))
+(defonce app-state* (r/atom {:count 0 :show-counter-with-state true}))
 (def count* (cursor app-state* [:count]))
+(def show-counter-with-state* (cursor app-state* [:show-counter-with-state]))
 
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
@@ -45,6 +46,18 @@
 (defn inc-button []
   [:button {:on-click #(swap! count* inc)} "+1"])
 
+(defn counter-with-state [id]
+  (let [state (r/atom 0)]
+    (add-watch
+     state
+     :watch
+     (fn [_ _ _ new-state]
+       (println (str "Atom " id " changed: " new-state))))
+    (fn []
+      [:div
+       [:div @state]
+       [:button {:on-click #(swap! state inc)} "+1"]])))
+
 (defn app []
   [:div
    [inc-button]
@@ -53,6 +66,10 @@
    [counter-with-let]
    [counter-with-track]
    [counter-with-reaction]
-   [counter-with-formatting-fn]])
+   [counter-with-formatting-fn]
+   [:hr]
+   [:div "add-watch test"]
+   [:button {:on-click #(swap! show-counter-with-state* not)} "toggle"]
+   (when @show-counter-with-state* [counter-with-state "id1"])])
 
-(r/render app (.getElementById js/document "app"))
+(r/render [app] (.getElementById js/document "app"))
